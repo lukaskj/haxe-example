8d953dc03a9de345bdf5bdbe49c5e7c2c93bff82
Lukas Jacomin <lukasjacomin@gmail.com>
2021-01-07 18:17:13 -0300

    Thread example

--- src/Main.hx
+++ src/Main.hx
@@ -1,7 +1,16 @@
 class Main {
-	static function main() {
-      trace('Simple test');
-      var test: Array<Int> = [1,2,3,4];
-      trace(test);
+	static function main():Void {
+		trace('Simple test');
+		var test:Array<Int> = [1, 2, 3, 4];
+		trace(test);
+		#if (target.threaded)
+		sys.thread.Thread.create(() -> {
+			while (true) {
+				trace("other thread");
+				Sys.sleep(1);
+			}
+		});
+		Sys.sleep(3);
+		#end
 	}
 }
